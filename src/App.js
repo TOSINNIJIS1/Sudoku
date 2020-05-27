c88adaa0a9b6d142db9804b88610e8fe6ac57dc1
VINCENT ADENIJI <adenijitosin112@gmail.com>
2020-05-26 20:18:00 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Soon from './soonPage/soon';
 
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-          <h1> WE KNOW YOU'RE EXCITED </h1>
-          <p>Omega 2020 sudoku is </p>
-          <h1> COMING SOON </h1>
-          <p> In the meantime <b>subscribe</b> to get alerted when the game is available </p>
 
-          <section className="button"> 
-            <p> subscribe </p>
-            
-            <p> learn how to play! </p>
-
-          </section>
-
-          
+      <Soon />
+        
     </div>
   );
 }
